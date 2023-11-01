ccc13d402919633de0f944cc67955e81955afd3a
Swift-ya <mingahigim@gmail.com>
2023-11-01 20:38:02 +0900

    ksjhdfkjshdfkjh

---
+++ 2_var.py
@@ -0,0 +1,50 @@
+# однострочный комментарий
+
+# "Ctrl + /" 
+
+'''mnogostrochnyi 
+commentaryi
+'''
+
+"""eto toge 
+mnogostrochnyi 
+commentaryi"""
+
+
+
+
+ # переменные
+
+ # snake_case
+var_1 = 15
+number_of_apples = 100
+
+#  camelcase 
+myNumVar = 20
+hhh = 555
+
+# константные переменные
+CONST_VAR = 1000
+
+
+# встроенная функция вывода в териминал
+# print(var_1)
+# print(myNumVar)
+# print(hhh)
+# print (hhh, CONST_VAR, number_of_apples)
+
+
+# встроенная функция ввода из терминала
+# in_1 = input("введите число: ")
+
+# print(in_1)
+
+# не большой калькулятор
+in_1 = input("ведите первое число ----")
+in_2 = input("ведите второе число ----")
+
+# result = in_1 + in_2
+
+result = int(in_1) + int(in_2)
+
+print(result)
